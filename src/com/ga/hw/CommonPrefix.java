4184e00f411ccf1d481265b41e47562abdd70a4b
Noor-Shafeeq-Ashoor <noorshafeeq2004@gmail.com>
2026-09-08 09:11:35 +0300

    Update CommonPrefix.java

--- src/com/ga/hw/CommonPrefix.java
+++ src/com/ga/hw/CommonPrefix.java
@@ -24,10 +24,8 @@ public class CommonPrefix {
 
     public static void main(String[] args) {
 
-        System.out.println(
-                longestCommonPrefix( new String[]{"flower", "flow", "flight"} ));
+        System.out.println( longestCommonPrefix( new String[]{"flower", "flow", "flight"} ));
 
-        System.out.println(
-                longestCommonPrefix( new String[]{"dog", "racecar", "car"}));
+        System.out.println( longestCommonPrefix( new String[]{"dog", "racecar", "car"}));
     }
 }
