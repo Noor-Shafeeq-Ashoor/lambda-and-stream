f650c278d6a8821ac4969423c227ae61fae1f00d
noor-ashoor <noorashoor2004@gmail.com>
2026-09-08 09:10:34 +0300

    update

--- src/com/ga/hw/CommonPrefix.java
+++ src/com/ga/hw/CommonPrefix.java
@@ -3,11 +3,31 @@ package com.ga.hw;
 public class CommonPrefix {
 
     public static String longestCommonPrefix(String[] words) {
-        return null;
+        if (words == null || words.length == 0) {return "";}
+
+        StringBuilder prefix = new StringBuilder();
+
+        for (int i = 0; i < words[0].length(); i++) {
+            char currentChar = words[0].charAt(i);
+
+            for (int j = 1; j < words.length; j++) {
+                if (i >= words[j].length()
+                        || words[j].charAt(i) != currentChar) {
+
+                    return prefix.toString();
+                }
+            }
+            prefix.append(currentChar);
+        }
+        return prefix.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
-        System.out.println(longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+
+        System.out.println(
+                longestCommonPrefix( new String[]{"flower", "flow", "flight"} ));
+
+        System.out.println(
+                longestCommonPrefix( new String[]{"dog", "racecar", "car"}));
     }
 }
